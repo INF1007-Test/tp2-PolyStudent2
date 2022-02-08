4e4740d6b2d4e4c1bc93072f054d9f8002281283
SamiSadfa <55071229+SamiSadfa@users.noreply.github.com>
2022-02-08 00:46:33 -0500

    hum

--- exercice4.py
+++ exercice4.py
@@ -1,45 +1,63 @@
-# Fontion fournie ne pas modifier
 clearConsole = lambda: print('\n' * 10)
 
+
 def print_table(table):
-    # Dimension 1 = ligne
-    # Dimension 2 = colonne
+    # Dimention 1 = ligne (y)
+    # Dimention 2 = colone (x)
     clearConsole()
     for i, row in enumerate(table):
         row_str = "{:>2}" * len(row)
         row_str = row_str.format(*row)
         print("{:^20}".format(row_str), '\n')
 
-# ________________________________________________________________
 
-# Partie 1
 def init_maze(nb_row, nb_col, player_pos, end_pos, walls):
-    maze = ... # TODO Générer un labyrinthe vide -> rempli de "_"
-
-    # TODO Placer le joueur "O" la sortie "X" les murs "W"
+    maze = [["_" for j in range(nb_col)] for i in range(nb_row)]
+    maze[player_pos[0]][player_pos[1]] = "O"
+    maze[end_pos[0]][end_pos[1]] = "X"
+    for wall in walls:
+        maze[wall[0]][wall[1]] = "W"
 
     return maze
 
-# Partie 2
+
 def validate_move(maze, new_player_pos):
-    result = False
-    # TODO Vérifier si la position est valide -> dans le labyrinthe et pas sur un mur
-    return result
+    if (0 <= new_player_pos[0] < len(maze) and
+            0 <= new_player_pos[1] < len(maze[0]) and
+            maze[new_player_pos[0]][new_player_pos[1]] != "W"):
+        return True
+    else:
+        return False
 
-#Partie 3
+
 def move(key_pressed, maze, player_pos):
-    move_dic = ... # TODO Créer le dictionnaire d'équivalence entre la touche appuyée et la direction ("up", "left", "down", "right")
+    move_dic = {"w": "up",
+                "a": "left",
+                "s": "down",
+                "d": "right"}
 
-    # TODO Vérifier si la touche appuyée est dans le dictionnaire
-    if (...) :
-        move = ... # TODO Récupérer la direction du mouvement
+    if key_pressed in move_dic:
+        move = move_dic[key_pressed]
 
-        # TODO Générer la position potentielle du joueur en fonction de la direction
-        new_player_pos = ...
+        new_player_pos = player_pos.copy()
+        if move == "up":
+            new_player_pos[0] -= 1
 
+        elif move == "down":
+            new_player_pos[0] += 1
+
+        elif move == "left":
+            new_player_pos[1] -= 1
+
+        elif move == "right":
+            new_player_pos[1] += 1
+
+        var = validate_move(maze, new_player_pos)
+        print(var)
         if validate_move(maze, new_player_pos):
-            # TODO Changer réellement la position du joueur
-            pass
+            maze[player_pos[0]][player_pos[1]] = "_"
+            player_pos = new_player_pos.copy()
+            maze[player_pos[0]][player_pos[1]] = "O"
 
     return maze, player_pos
 
@@ -47,24 +65,16 @@ def move(key_pressed, maze, player_pos):
 if __name__ == '__main__':
     nb_row = 4
     nb_col = 7
-    player_pos = ...  # TODO Définir la position ligne, colonne sur en haut à gauche
-    end_pos = ...  # TODO Définir la position ligne, colonne sur en bas à droite
-    # Coordoné sous la forme (ligne, colone)
-    walls = [[0, 1],[1, 1], [1, 2], [1, 3], [1, 5], [2, 1], [2, 5], [3, 3], [3, 5]]
+    player_pos = [0, 0]
+    end_pos = [nb_row - 1, nb_col - 1]
+    walls = [[0, 1], [1, 1], [1, 2], [1, 3], [1, 5], [2, 1], [2, 5], [3, 3], [3, 5]]
     maze = init_maze(nb_row, nb_col, player_pos, end_pos, walls)
 
     print_table(maze)
 
-    # TODO Décommenter pour tester votre code en Partie 2
-    # test_pos = [-1,0] changez les valeurs pour tester tous les cas possibles
-    # valid = validate_move(maze, test_pos)
-    # print(valid)
+    while player_pos != end_pos:
+        key_pressed = input("mouvement : ")
+        maze, player_pos = move(key_pressed, maze, player_pos)
+        print_table(maze)
 
-
-    # TODO Décommenter pour tester votre code en Partie 3
-    # while player_pos != end_pos:
-    #     key_pressed = input("mouvement : ")
-    #     maze, player_pos = move(key_pressed, maze, player_pos)
-    #     print_table(maze)
-    #
-    # print("Vous avez gagnez !")
+    print("Vous avez gagnez !")
